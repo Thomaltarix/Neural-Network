218cad5267344f12337dfb8c33f7a7a46c1bec25
thomaltarix <thomas.boue@epitech.eu>
2024-12-06 17:52:51 +0100

    fix(generator): Fix tests compilation

--- torch_generator/tests/test.rs
+++ torch_generator/tests/test.rs
@@ -6,7 +6,7 @@ fn test_configuration_new() {
     \"input_by_input_neuron\": 2,\
     \"bias\": 0.5,\
     \"learning_rate\": 0.1,\
-    \"layers\": [2, 3, 1]}");
+    \"layers\": [2, 3, 1]}".to_string());
     assert_eq!(config.input_by_input_neuron, 2);
     assert_eq!(config.bias, 0.5);
     assert_eq!(config.learning_rate, 0.1);
