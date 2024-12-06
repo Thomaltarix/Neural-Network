71b25d21f87705158a1059342cec89bf6d197ec1
thomaltarix <thomas.boue@epitech.eu>
2024-12-06 17:21:41 +0100

    feat(generator): Add some tests for the configuration class

---
+++ torch_generator/tests/test.rs
@@ -0,0 +1,14 @@
+use my_torch_generator::models::Configuration;
+
+#[test]
+fn test_configuration_new() {
+    let config = Configuration::new("{\
+    \"input_by_input_neuron\": 2,\
+    \"bias\": 0.5,\
+    \"learning_rate\": 0.1,\
+    \"layers\": [2, 3, 1]}");
+    assert_eq!(config.input_by_input_neuron, 2);
+    assert_eq!(config.bias, 0.5);
+    assert_eq!(config.learning_rate, 0.1);
+    assert_eq!(config.layers, vec![2, 3, 1]);
+}
