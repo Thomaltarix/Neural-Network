870631e1b3fdf6748d63c7f641a14fa8d198df5b
thomaltarix <thomas.boue@epitech.eu>
2024-12-06 18:33:02 +0100

    feat(generator): Add a test for the Neural Network model

--- torch_generator/tests/test.rs
+++ torch_generator/tests/test.rs
@@ -1,4 +1,6 @@
 use my_torch_generator::models::Configuration;
+use my_torch_generator::models::NeuralNetwork;
+use my_torch_generator::models::Neuron;
 
 #[test]
 fn test_configuration_new() {
@@ -11,4 +13,20 @@ fn test_configuration_new() {
     assert_eq!(config.bias, 0.5);
     assert_eq!(config.learning_rate, 0.1);
     assert_eq!(config.layers, vec![2, 3, 1]);
+}
+
+#[test]
+fn test_neural_network_new() {
+    let nn = NeuralNetwork::new(0.1, vec![vec![
+        Neuron::new(vec![0.1, 0.1], 0.5, 0.6),
+        Neuron::new(vec![0.1, 0.1], 0.5, 0.6)
+    ]]);
+    assert_eq!(nn.learning_rate, 0.1);
+    for layer in nn.layers {
+        for neuron in layer {
+            assert_eq!(neuron.weights, vec![0.1, 0.1]);
+            assert_eq!(neuron.bias, 0.5);
+            assert_eq!(neuron.activation, 0.6);
+        }
+    }
 }
