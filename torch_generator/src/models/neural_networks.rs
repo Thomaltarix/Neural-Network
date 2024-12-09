5c8ea99942f4b2c4ca4147aef5fd58a10a79a6d0
thomaltarix <thomas.boue@epitech.eu>
2024-12-09 16:00:36 +0100

    feat(generator): Fill files with the right Neural Network content
    Now, from a configuration file (.conf), you can generate a Neural Network file (.nn)

--- torch_generator/src/models/neural_networks.rs
+++ torch_generator/src/models/neural_networks.rs
@@ -1,16 +1,18 @@
 use core::fmt;
 use std::fmt::Formatter;
 use serde::{Deserialize, Serialize};
+use crate::models::Configuration;
+use rand::Rng;
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct Neuron {
     pub weights: Vec<f64>,
     pub bias: f64,
-    pub activation: f64
+    pub activation: String
 }
 
 impl Neuron {
-    pub fn new(weights: Vec<f64>, bias: f64, activation: f64) -> Self {
+    pub fn new(weights: Vec<f64>, bias: f64, activation: String) -> Self {
         Neuron {
             weights,
             bias,
@@ -32,11 +34,29 @@ pub struct NeuralNetwork {
 }
 
 impl NeuralNetwork {
-    pub fn new(learning_rate: f64, layers: Vec<Vec<Neuron>>) -> Self {
+    pub fn new(config: Configuration) -> Self {
+        let mut layers: Vec<Vec<Neuron>> = Vec::new();
+        for i in 0..config.layers.len() {
+            let mut neurons: Vec<Neuron> = Vec::new();
+            for _ in 0..config.layers[i] {
+                let mut weights: Vec<f64> = Vec::new();
+                for _ in 0..config.input_by_input_neuron {
+                    let mut rng = rand::thread_rng();
+                    let value : f64 = rng.gen();
+                    weights.push(value);
+                }
+                neurons.push(Neuron::new(weights, config.bias, config.activation.clone()));
+            }
+            layers.push(neurons);
+        }
         NeuralNetwork {
-            learning_rate,
+            learning_rate: config.learning_rate,
             layers
         }
+    }
+
+    pub fn get_json(&self) -> String {
+        serde_json::to_string_pretty(&self).unwrap()
     }
 }
 
